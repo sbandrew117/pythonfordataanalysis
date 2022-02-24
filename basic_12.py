0bf6f8446b0339364310b6b915c719e5441ca748
Kyung Jun Kim <sbandrew117@ex-em.com>
2022-02-24 17:13:39 +0900

    ~ page 629

--- basic_12.py
+++ basic_12.py
@@ -86,14 +86,77 @@ def take():
     
     
     #브로드캐스팅은 다른 모양의 배열 간의 산술 연산 수행.
-def broadcast():
+    #page 602 - page 607 복습
+
+
+def ufunc():
+    arr = np.arange(10)
+    print(np.add.reduce(arr)) #np.add.reduce() -> 배열의 모든 원소 더하는 방법
+    
+    print(arr.sum()) #.sum() -> 배열의 모든 원소 더하는 방법
+    
+def dtype():
+    dtype = [('x', np.float64), ('y', np.int32)]
+    sarr = np.array([(1.5, 6), (np.pi, -2)], dtype = dtype)
+    print("\nsarr:\n", sarr)    
+        
+    print("\nsarr[0]:\n", sarr[0])
+    
+    print("\nsarr[0]['y']:\n", sarr[0]['y'])
+    
+def sort():
+    #argsort and lexsort
+    values = np.array([5, 0, 1, 3, 2])
+    indexer = values.argsort()
+    
+    print("\nby index:\n", indexer) #argsort() -> sorts by index
+    
+    print("\nsorted values of index:\n", values[indexer]) #sorts by values of index
+    
+    #two dimensional example
+    arr = np.random.randn(3, 5)
+    arr[0] = values
+    
+    print("\nrandom 3 by 5\n", arr) #0th index is "values"
+    
+    first_name = np.array(['Bob', 'Jane', 'Steve', 'Bill', 'Barbara'])
+    last_name = np.array(['Jones', 'Arnold', 'Arnold', 'Jones', 'Walters'])
+    sorter = np.lexsort((first_name, last_name))
+    print(sorter)
+    
+def stable():
+    values = np.array(['2 : first', '2 : second', '1 : first',
+                       '1 : second', '1 : third'])    
+    key = np.array([2, 2, 1, 1, 1])
+    
+    indexer = key.argsort(kind = 'mergesort')
+    
+    print(indexer)
+    
+    print(values.take(indexer))
+    
+def partial():
+    np.random.seed(12345)
+    arr = np.random.randn(20)
+    print("\nprint the smallest three in the front:\n",
+          np.partition(arr, 3)) #첫 세 원소는 해당 배열에서 가장 작은 값
+    indices = np.argpartition(arr, 3) #해당 원소의 위치를 반환
+    print(indices)
+    print(arr.take(indices))
+    
+def search():
+    arr = np.array([0, 1, 7, 12, 15])
+    print(arr.searchsorted(9)) #새로운 값을 삽입할 때 정렬된 상태를
+    #계속 유지하기 위한 위치를 반환
+    
+    print(arr.searchsorted([0, 8, 11, 16]))
+
+    arr1 = np.array([0, 0, 0, 1, 1, 1, 1])
+    print(arr1.searchsorted([0, 1]))
+    print(arr1.searchsorted([0, 1], side = 'right'))
     
     
-    
-    
-    
-    
-    
+
     
 if __name__ == "__main__":
     #reshape()
@@ -101,4 +164,10 @@ if __name__ == "__main__":
     #array_02()
     #array_03()
     #take()
-    broadcast()
+    #ufunc()
+    #dtype()
+    #sort()
+    #stable()
+    #partial()
+    search()
+    
